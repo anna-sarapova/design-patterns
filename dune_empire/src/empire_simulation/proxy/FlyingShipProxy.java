8a39d465e43259ef9a44c34f4bef6ab2be06df7b
John Wick <sarapovaaniuta@gmail.com>
2021-11-11 23:09:28 +0200

    add proxy design pattern

---
+++ dune_empire/src/empire_simulation/proxy/FlyingShipProxy.java
@@ -0,0 +1,17 @@
+package empire_simulation.proxy;
+
+public class FlyingShipProxy extends FlyingShip{
+
+    private static  final int NUM_PEOPLE_ALLOWED = 5;
+    private int numPeople;
+
+    @Override
+    public void onBoard(People people){
+        if (numPeople < NUM_PEOPLE_ALLOWED){
+            super.onBoard(people);
+            numPeople++;
+        } else {
+            System.out.println( people + " is not allowed to enter.");
+        }
+    }
+}
